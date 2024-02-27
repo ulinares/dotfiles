502c78f56cc9b7eafb2d037010419adc6144a0d2
Uziel Linares <ulinares@quinstreet.com>
2024-02-27 17:26:22 -0600

    add option left/right keystrokes to jump between words

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -12,15 +12,22 @@ config.font_size = 13
 
 config.color_scheme = 'Tokyo Night'
 
-config.window_background_image = os.getenv( "HOME" ) .. "/.config/wezterm/red-spider-nebula.jpg"
+-- config.window_background_image = os.getenv( "HOME" ) .. "/.config/wezterm/cyberpunk.jpg"
 
 config.window_background_image_hsb = {
-    brightness = 0.2,
+    brightness = 0.3,
     hue = 1.0,
     saturation = 1.0,
 }
 
-config.window_background_opacity = 1.0
-config.text_background_opacity = 1.
+config.text_background_opacity = 0.9
+config.window_background_opacity = 0.9
+
+config.keys = {
+    -- Make Option-Left equivalent to Alt-b which many line editors interpret as backward-word
+    {key="LeftArrow", mods="OPT", action=wezterm.action{SendString="\x1bb"}},
+    -- Make Option-Right equivalent to Alt-f; forward-word
+    {key="RightArrow", mods="OPT", action=wezterm.action{SendString="\x1bf"}},
+}
 
 return config
